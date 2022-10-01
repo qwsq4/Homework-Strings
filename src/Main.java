4b6844ad3476f05b0a6f679160c80710eae0843e
qwsq4 <qwsq1437@gmail.com>
2022-10-01 20:56:26 +0800

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        System.out.println("  Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
 
+        //Задание 2
+        System.out.println("  Задание 2");
+        System.out.println("ФИО сотрудника - " + fullName.toUpperCase());
+
+        //Задание 3
+        System.out.println("  Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("ФИО сотрудника - " + fullName);
     }
 }
